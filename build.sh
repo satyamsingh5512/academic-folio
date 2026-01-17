5c17d10c5a2b40233f992004c1f4e91d41928a86
satyamsingh5512 <satyamsinghnew5512@gmail.com>
2026-01-18 00:22:02 +0530

    Use curl instead of wget for Vercel build

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 # Download and install Hugo
 HUGO_VERSION=0.83.1
-wget -q https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
-tar -xzf hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+curl -L -o hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
+tar -xzf hugo.tar.gz
 chmod +x hugo
 
 # Build the site
